8226b7481135c2c68af43a45ab72764defa7c01c
Cris <cristhianeliassales@gmail.com>
2025-10-15 09:52:21 -0300

    faq acordion completo

--- faq-acordion/script.js
+++ faq-acordion/script.js
@@ -14,3 +14,37 @@ function verMais(num) {
         aberto[num - 1] = false;
     }
 }
+
+const perguntas = document.querySelectorAll('.perguntas');
+let indiceAtual = 0;
+let navegou = false;
+
+document.addEventListener('keydown', function(event) {
+
+    if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+        if (navegou) {
+            perguntas[indiceAtual].classList.remove('selecionada');
+        }
+
+        navegou = true;
+
+        if (event.key === 'ArrowUp' && indiceAtual > 0) {
+            indiceAtual = indiceAtual - 1;
+        }
+
+        if (event.key === 'ArrowDown' && indiceAtual < perguntas.length - 1) {
+            indiceAtual = indiceAtual + 1;
+        }
+
+        perguntas[indiceAtual].classList.add('selecionada');
+    }
+
+    if (event.key === 'Enter' || event.key === ' ') {
+        if (navegou) {
+            let paragrafo = perguntas[indiceAtual].querySelector('p.bloco');
+            if (paragrafo) {
+                paragrafo.classList.toggle('vis');
+            }
+        }
+    }
+});
