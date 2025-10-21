8454715e66a308a6f7055f5b558e75ad3af026de
Cris <cristhianeliassales@gmail.com>
2025-10-21 11:09:25 -0300

    .

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,8 @@
+let tela1 = document.getElementById('p-tela')
+let tela2 = document.getElementById('s-tela')
+
+function enviado() {
+    tela1.style.setProperty('display', 'none');
+    tela2.style.setProperty('display', 'block');
+
+}
